aad8be9c2a6a7b29bf08cec1d2b439aab3ce0289
Francis Galiegue <fgaliegue@gmail.com>
2011-07-13 01:25:15 +0200

    Javadoc for eel/kitchen/jsonschema/validators/errors/*

--- src/main/java/eel/kitchen/jsonschema/validators/errors/TypeMismatchValidator.java
+++ src/main/java/eel/kitchen/jsonschema/validators/errors/TypeMismatchValidator.java
@@ -4,9 +4,29 @@ import eel.kitchen.jsonschema.validators.AbstractValidator;
 
 import java.util.List;
 
+/**
+ * <p>Utility validator, always triggering a validation failure. This validator
+ * will be spawned when:</p>
+ *
+ * <ul>
+ *     <li>the given schema will not allow any type at all (for instance:
+ *     <code>"disallow": "any"</code>, but also <code>"type":
+ *     "integer"</code> combined with <code>"disallow": "number"</code></li>;
+ *     <li>the given schema allows for one, or a set of, certain node types,
+ *     but the node to be validated is neither of those.
+ *     </li>
+ * </ul>
+ *
+ */
 public final class TypeMismatchValidator
     extends AbstractValidator
 {
+    /**
+     * Constructor for this particular validator.
+     *
+     * @param types The list of allowed types for this validator, may be empty
+     * @param actual The type of the node which is to be validated
+     */
     public TypeMismatchValidator(final List<String> types, final String actual)
     {
         final String message;
