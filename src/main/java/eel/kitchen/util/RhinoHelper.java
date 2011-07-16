dc6c0973bbd2c0e3b1f459e41cd9de206d3dea51
Francis Galiegue <fgaliegue@gmail.com>
2011-07-16 17:47:27 +0200

    Javadoc for RhinoHelper, and chastize Java's misuse of "matching"
    This is all the more incomprehensible that James Gosling is far from being an
    idiot. But heh, File also doesn't throw an exception when you can't .delete().

--- src/main/java/eel/kitchen/util/RhinoHelper.java
+++ src/main/java/eel/kitchen/util/RhinoHelper.java
@@ -21,15 +21,45 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+/**
+ * <p>ECMA 262 validation helper. Rhino is used in lieu of java.util.regex
+ * because the latter doesn't comply with ECMA 262:</p>
+ *
+ * <ul>
+ *     <li>ECMA 262 doesn't have PATTERN_DOTALL;</li>
+ *     <li>ECMA 262 doesn't have "possessive" quantifiers (<code>++</code>,
+ *     <code>?+</code>, etc);</li>
+ *     <li>the word delimiter in ECMA 262 is <code>\b</code>,
+ *     it does not understand <code>\&lt;</code> or <code>\&gt;</code>.</li>
+ * </ul>
+ *
+ * <p>And many, many other things. See
+ * <a href="http://www.regular-expressions.info/javascript.html">here</a>
+ * for the full story. And if you don't yet have Jeffrey Friedl's "Mastering
+ * regular expressions", just <a href="http://regex.info">buy it</a> :p</p>
+ */
 public final class RhinoHelper
 {
+    /**
+     * The two scriptlets which will be used for regex format validation,
+     * and matching validation respectively.
+     */
     private static final String
         REGEX_FORMAT = "/%s/",
         REGEX_VALIDATE = "/%s/.test(\"%s\")";
 
+    /**
+     * Instance of Rhino's {@link ScriptEngine} to use.
+     */
     private static final ScriptEngine engine
         = new ScriptEngineManager().getEngineByName("JavaScript");
 
+    /**
+     * Validate that a regex is in good shape.
+     *
+     * @param regex the regex to validate
+     * @return true if the regex is valid
+     */
     public static boolean regexIsValid(final String regex)
     {
         try {
@@ -40,6 +70,20 @@ public final class RhinoHelper
         }
     }
 
+    /**
+     * <p>Matches an input against a given regex. Note that Java in general
+     * makes a HUGE mistake about the meaning of the word "matching": a regex
+     * matches <i>anywhere in the input</i>. <b>NOTHING has ever said that
+     * "matching" an input should match the WHOLE INPUT. String's (and
+     * Matcher's) .matches() methods are MISNOMERS.</b> This method does the
+     * real thing. If you want your regex to be anchored, make it so!</p>
+     *
+     * <p>Also note that the regex MUST have been validated at this point.</p>
+     *
+     * @param regex the regex to use
+     * @param input the input to match against (and again, see description)
+     * @return true if the regex matches the input
+     */
     public static boolean regMatch(final String regex, final String input)
     {
         final String js = String.format(REGEX_VALIDATE, regex, input);
