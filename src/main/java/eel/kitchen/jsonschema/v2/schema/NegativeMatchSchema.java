4c20837e8174b6cc1215537be74ca5a0435a56c7
Francis Galiegue <fgaliegue@gmail.com>
2011-10-25 23:44:51 +0200

    Implement negative match, match any and match all
    Not sure for the "winner" argument though: it may be useless, I don't know yet.

---
+++ src/main/java/eel/kitchen/jsonschema/v2/schema/NegativeMatchSchema.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2011, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eel.kitchen.jsonschema.v2.schema;
+
+import eel.kitchen.jsonschema.v2.instance.Instance;
+
+public final class NegativeMatchSchema
+    extends AbstractSchema
+{
+    private final Schema schema;
+
+    public NegativeMatchSchema(final Schema schema)
+    {
+        this.schema = schema;
+    }
+
+    @Override
+    public Schema getSchema(final String path)
+    {
+        return schema.getSchema(path);
+    }
+
+    @Override
+    public boolean validate(final Instance instance)
+    {
+        if (!schema.validate(instance))
+            return true;
+
+        messages.add("instance matches a forbidden schema");
+        return false;
+    }
+}
