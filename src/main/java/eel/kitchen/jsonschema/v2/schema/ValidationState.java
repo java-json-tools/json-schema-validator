1684d8803c636eaac83da645fb3803179606dd43
Francis Galiegue <fgaliegue@gmail.com>
2011-10-29 13:02:08 +0200

    New ValidationState class
    This class will be passed along all validators. It will retain the current
    validation state, but also the next schema to validate if ValidationStatus is
    DUNNO.

---
+++ src/main/java/eel/kitchen/jsonschema/v2/schema/ValidationState.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2011, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eel.kitchen.jsonschema.v2.schema;
+
+import eel.kitchen.jsonschema.v2.keyword.ValidationStatus;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import static eel.kitchen.jsonschema.v2.keyword.ValidationStatus.*;
+
+public final class ValidationState
+{
+    private SchemaFactory factory;
+    private final List<String> messages = new LinkedList<String>();
+    private ValidationStatus status = DUNNO;
+    private Schema nextSchema;
+
+    public SchemaFactory getFactory()
+    {
+        return factory;
+    }
+
+    public void setFactory(final SchemaFactory factory)
+    {
+        this.factory = factory;
+    }
+
+    public void addMessage(final String message)
+    {
+        messages.add(message);
+    }
+
+    public void addMessages(final List<String> messages)
+    {
+        this.messages.addAll(messages);
+    }
+
+    public void clearMessages()
+    {
+        messages.clear();
+    }
+
+    public List<String> getMessages()
+    {
+        return Collections.unmodifiableList(messages);
+    }
+
+    public ValidationStatus getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(final ValidationStatus status)
+    {
+        this.status = status;
+        if (status != DUNNO)
+            nextSchema = null;
+    }
+
+    public boolean isFailure()
+    {
+        return status == FAILURE || status == ERROR;
+    }
+    public Schema getNextSchema()
+    {
+        if (nextSchema == null)
+            throw new RuntimeException(".getNextSchema() called without a "
+                + "schema to give back");
+        return nextSchema;
+    }
+
+    public void setNextSchema(final Schema nextSchema)
+    {
+        this.nextSchema = nextSchema;
+    }
+}
