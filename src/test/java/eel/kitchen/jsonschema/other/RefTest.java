df0f7f57b218b0767e032ab76caa7e64c007e0bf
Francis Galiegue <fgaliegue@gmail.com>
2011-11-06 09:00:59 +0100

    Update copyright: LGPLv3, not GPLv3

--- src/test/java/eel/kitchen/jsonschema/other/RefTest.java
+++ src/test/java/eel/kitchen/jsonschema/other/RefTest.java
@@ -1,10 +1,10 @@
-/**
+/*
  * Copyright (c) 2011, Francis Galiegue <fgaliegue@gmail.com>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Lesser GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
