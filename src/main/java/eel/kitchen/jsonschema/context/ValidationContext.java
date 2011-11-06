f2c2ab8b82b73704b2b90b579d13e9db011885da
Francis Galiegue <fgaliegue@gmail.com>
2011-11-06 10:31:41 +0100

    Javadoc for ValidationContext

--- src/main/java/eel/kitchen/jsonschema/context/ValidationContext.java
+++ src/main/java/eel/kitchen/jsonschema/context/ValidationContext.java
@@ -17,30 +17,77 @@
 
 package eel.kitchen.jsonschema.context;
 
+import eel.kitchen.jsonschema.JsonValidator;
 import eel.kitchen.jsonschema.ValidationReport;
 import eel.kitchen.jsonschema.base.AlwaysFalseValidator;
 import eel.kitchen.jsonschema.base.Validator;
 import eel.kitchen.jsonschema.factories.KeywordFactory;
 import eel.kitchen.jsonschema.factories.SyntaxFactory;
+import eel.kitchen.jsonschema.keyword.RefValidator;
+import eel.kitchen.jsonschema.syntax.SyntaxValidator;
 import eel.kitchen.util.RefResolver;
 import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.MissingNode;
 
 import java.io.IOException;
 
+/**
+ * <p>Class passed to all {@link Validator} implementations. This class is
+ * responsible for several things:</p>
+ * <ul>
+ *     <li>checking the schema correctness (using {@link SyntaxValidator}
+ *     instances);</li>
+ *     <li>create validator instances;</li>
+ *     <li>resolve {@code $ref} (see {@link RefValidator});</li>
+ *     <li>provide {@link ValidationReport} instances;</li>
+ *     <li>provide other instances of itself.</li>
+ * </ul>
+ */
 public final class ValidationContext
 {
+    /**
+     * The root schema of this validation context
+     */
     private JsonNode rootSchema;
+
+    /**
+     * The schema used by the current context
+     */
     private JsonNode schemaNode;
+
+    /**
+     * The JSON path within the instance for the current context
+     */
     private String path;
 
+    /**
+     * The keyword validator factory
+     */
     private KeywordFactory keywordFactory;
+
+    /**
+     * The syntax validator factory
+     */
     private SyntaxFactory syntaxFactory;
+
+    /**
+     * The $ref resolver
+     */
     private RefResolver refResolver;
 
+    /**
+     * The default constructor, which is private by design
+     */
     private ValidationContext()
     {
     }
 
+    /**
+     * The public constructor. Only used from {@link JsonValidator}. On
+     * initial setup, the argument is the root schema, see #rootSchema.
+     *
+     * @param schemaNode the root schema used by this context
+     */
     public ValidationContext(final JsonNode schemaNode)
     {
         path = "#";
@@ -51,11 +98,23 @@ public final class ValidationContext
         refResolver = new RefResolver(schemaNode);
     }
 
+    /**
+     * Return the schema node of this context -- <b>not</b> the root schema!
+     *
+     * @return the matching {@link JsonNode}
+     */
     public JsonNode getSchemaNode()
     {
         return schemaNode;
     }
 
+    /**
+     * Spawn a new context from this context.
+     *
+     * @param subPath the relative path to use from the current #path
+     * @param subSchema the schema node to use for this context
+     * @return the new context
+     */
     public ValidationContext createContext(final String subPath,
         final JsonNode subSchema)
     {
@@ -73,11 +132,27 @@ public final class ValidationContext
         return other;
     }
 
+    /**
+     * Shortcut to call {@link #createContext(String, JsonNode)}
+     * with an empty path
+     *
+     * @param subSchema the schema node to use
+     * @return the new context
+     */
     public ValidationContext createContext(final JsonNode subSchema)
     {
         return createContext("", subSchema);
     }
 
+    /**
+     * Create a {@link Validator} for a given JSON instance. This is what
+     * MUST be called by validators when they need to spawn a new validator,
+     * because this method handles syntax checking. If the syntax of the
+     * schema itself is wrong, returns an {@link AlwaysFalseValidator}.
+     *
+     * @param instance the JSON instance
+     * @return the validator
+     */
     public Validator getValidator(final JsonNode instance)
     {
         final ValidationReport report = new ValidationReport(path);
@@ -92,16 +167,38 @@ public final class ValidationContext
         return keywordFactory.getValidator(this, instance);
     }
 
-    public ValidationReport createReport()
-    {
-        return createReport("");
-    }
-
+    /**
+     * Create a new report with, optionally, a prefix (this DOES NOT affect
+     * the path)
+     *
+     * @param prefix the prefix to use
+     * @return the newly created report
+     */
     public ValidationReport createReport(final String prefix)
     {
         return new ValidationReport(path + prefix);
     }
 
+    /**
+     * Shortcut to {@link #createReport(String)} with an empty prefix
+     *
+     * @return the newly created report
+     */
+    public ValidationReport createReport()
+    {
+        return createReport("");
+    }
+
+    /**
+     * Resolve a {@code $ref} relatively to #rootSchema. Used by {@link
+     * RefValidator}.
+     *
+     * @param path the path within the root schema
+     * @return the {@link JsonNode}, which is a {@link MissingNode} if the
+     * path does not exist
+     * @throws IOException the ref points to an URL, and the JSON schema at
+     * this URL could not be downloaded
+     */
     public JsonNode resolve(final String path)
         throws IOException
     {
