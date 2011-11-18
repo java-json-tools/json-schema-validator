0d67fb18ac41dc92d3e6d8331a89366450fc1125
Francis Galiegue <fgaliegue@gmail.com>
2011-11-19 00:22:17 +0100

    Preliminary feature test
    Which confirms that everything works as expected: it fails on the first item.

---
+++ src/test/java/org/eel/kitchen/jsonschema/mechanics/FeaturesTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2011, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eel.kitchen.jsonschema.mechanics;
+
+import org.codehaus.jackson.JsonNode;
+import org.eel.kitchen.jsonschema.main.JsonValidationFailureException;
+import org.eel.kitchen.jsonschema.main.JsonValidator;
+import org.eel.kitchen.jsonschema.main.ValidationFeature;
+import org.eel.kitchen.util.JsonLoader;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.*;
+
+public final class FeaturesTest
+{
+    private JsonNode testNode;
+
+    @BeforeClass
+    public void setUp()
+        throws IOException
+    {
+        testNode = JsonLoader.fromResource("/container/array.json")
+            .get("items");
+    }
+
+    @Test
+    public void testFailFast()
+    {
+        final JsonValidator validator
+            = new JsonValidator(testNode.get("schema"));
+        validator.setFeature(ValidationFeature.FAIL_FAST);
+
+        try {
+            validator.validate(testNode.get("bad"));
+            fail("No exception thrown");
+        } catch (JsonValidationFailureException e) {
+            assertEquals(e.getMessage(), testNode.get("messages").get(0)
+                .getTextValue());
+        }
+    }
+}
