fb3fbcd99b1ca4a6a23d939094c71fa2aaf4866e
Francis Galiegue <fgaliegue@gmail.com>
2012-04-28 03:40:24 +0200

    Get rid of unused code

--- src/main/java/org/eel/kitchen/jsonschema/main/JsonSchemaException.java
+++ src/main/java/org/eel/kitchen/jsonschema/main/JsonSchemaException.java
@@ -27,23 +27,4 @@ public final class JsonSchemaException
     {
         super(message);
     }
-
-    /**
-     * Constructs a new exception with the specified detail message and
-     * cause.  <p>Note that the detail message associated with
-     * <code>cause</code> is <i>not</i> automatically incorporated in
-     * this exception's detail message.
-     *
-     * @param message the detail message (which is saved for later retrieval
-     *                by the {@link #getMessage()} method).
-     * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link #getCause()} method).  (A <tt>null</tt> value is
-     *                permitted, and indicates that the cause is nonexistent or
-     *                unknown.)
-     * @since 1.4
-     */
-    public JsonSchemaException(final String message, final Throwable cause)
-    {
-        super(message, cause);
-    }
 }
--- src/main/java/org/eel/kitchen/jsonschema/main/JsonValidationFailureException.java
+++ src/main/java/org/eel/kitchen/jsonschema/main/JsonValidationFailureException.java
@@ -20,10 +20,6 @@ package org.eel.kitchen.jsonschema.main;
 public final class JsonValidationFailureException
     extends Exception
 {
-    public JsonValidationFailureException()
-    {
-    }
-
     public JsonValidationFailureException(final String message)
     {
         super(message);
