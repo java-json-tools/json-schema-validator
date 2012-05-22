f7c8c1c5651944684e54c533a04c7f26959ea3c6
Francis Galiegue <fge@one2team.com>
2012-05-22 13:56:30 +0200

    Turn Issue7 into a (failing) test

--- src/test/java/org/eel/kitchen/Issue7.java
+++ src/test/java/org/eel/kitchen/Issue7.java
@@ -4,39 +4,47 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.eel.kitchen.jsonschema.main.ValidationReport;
 import org.eel.kitchen.jsonschema.schema.JsonSchema;
 import org.eel.kitchen.util.JsonLoader;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 
+import static org.testng.Assert.*;
+
 public final class Issue7
 {
-    public static void main(final String... args)
+    private JsonNode draftv3, schema1, schema2;
+
+    @BeforeClass
+    public void setUp()
         throws IOException
     {
-        final JsonNode draftv3 = JsonLoader.fromResource("/schema-draftv3.json");
-        final JsonNode schema1 = JsonLoader.fromResource("/schema1.json");
-        final JsonNode schema2 = JsonLoader.fromResource("/schema2.json");
+        draftv3 = JsonLoader.fromResource("/schema-draftv3.json");
+        schema1 = JsonLoader.fromResource("/schema1.json");
+        schema2 = JsonLoader.fromResource("/schema2.json");
 
+    }
+
+    @Test
+    public void testIssue7()
+    {
         final JsonSchema schema = JsonSchema.fromNode(draftv3);
-        ValidationReport valReport;
+        ValidationReport report;
 
-        System.out.println("TEST 1");
-        valReport = new ValidationReport();
-        schema.validate(valReport, schema1);
-        for (final String msg : valReport.getMessages())
-            System.out.println(msg);
+        report = new ValidationReport();
+        schema.validate(report, schema1);
+        assertTrue(report.isSuccess());
 
         final JsonSchema temp1schema = JsonSchema.fromNode(schema1);
 
-        System.out.println("TEST 2");
         /**
          * The bug is here: normally, validation should fail because
          * "indexed" is not a boolean. But it succeeds... The reason for this
          * is that JsonSchema's .objectPath() returns an empty schema: it
          * shouldn't!
          */
-        valReport = new ValidationReport();
-        temp1schema.validate(valReport, schema2);
-        for (final String msg : valReport.getMessages())
-            System.out.println(msg);
+        report = new ValidationReport();
+        temp1schema.validate(report, schema2);
+        assertFalse(report.isSuccess());
     }
 }
