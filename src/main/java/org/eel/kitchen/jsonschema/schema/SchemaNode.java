87e8123667c4937d192cf94709b5adb338ed2913
Francis Galiegue <fgaliegue@gmail.com>
2012-06-04 21:48:01 +0200

    SchemaNode: add object key schema lookup, plus tests

--- src/main/java/org/eel/kitchen/jsonschema/schema/SchemaNode.java
+++ src/main/java/org/eel/kitchen/jsonschema/schema/SchemaNode.java
@@ -19,9 +19,15 @@ package org.eel.kitchen.jsonschema.schema;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import org.eel.kitchen.util.CollectionUtils;
+import org.eel.kitchen.util.RhinoHelper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public final class SchemaNode
 {
@@ -35,33 +41,21 @@ public final class SchemaNode
 
     private final List<JsonNode> items = new ArrayList<JsonNode>();
 
+    private JsonNode additionalProperties = EMPTY_SCHEMA;
+
+    private final Map<String, JsonNode> properties
+        = new HashMap<String, JsonNode>();
+
+    private final Map<String, JsonNode> patternProperties
+        = new HashMap<String, JsonNode>();
+
     public SchemaNode(final SchemaContainer container, final JsonNode node)
     {
         this.container = container;
         this.node = node;
 
         setupArraySchemas();
-    }
-
-    private void setupArraySchemas()
-    {
-        JsonNode tmp;
-
-        tmp = node.path("items");
-
-        if (tmp.isObject()) {
-            additionalItems = tmp;
-            return;
-        }
-
-        if (tmp.isArray())
-            for (final JsonNode item: tmp)
-                items.add(item);
-
-        tmp = node.path("additionalItems");
-
-        if (tmp.isObject())
-            additionalItems = tmp;
+        setupObjectSchemas();
     }
 
     public SchemaContainer getContainer()
@@ -99,5 +93,63 @@ public final class SchemaNode
     public JsonNode getArraySchema(final int index)
     {
         return index < items.size() ? items.get(index) : additionalItems;
+    }
+
+    public Set<JsonNode> getObjectSchemas(final String key)
+    {
+        final Set<JsonNode> ret = new HashSet<JsonNode>();
+
+        if (properties.containsKey(key))
+            ret.add(properties.get(key));
+
+        for (final String regex: patternProperties.keySet())
+            if (RhinoHelper.regMatch(regex, key))
+                ret.add(patternProperties.get(regex));
+
+        if (ret.isEmpty())
+            ret.add(additionalProperties);
+
+        return ret;
+    }
+
+    private void setupArraySchemas()
+    {
+        JsonNode tmp;
+
+        tmp = node.path("items");
+
+        if (tmp.isObject()) {
+            additionalItems = tmp;
+            return;
+        }
+
+        if (tmp.isArray())
+            for (final JsonNode item: tmp)
+                items.add(item);
+
+        tmp = node.path("additionalItems");
+
+        if (tmp.isObject())
+            additionalItems = tmp;
+    }
+
+    private void setupObjectSchemas()
+    {
+        JsonNode tmp;
+
+        tmp = node.path("additionalProperties");
+
+        if (tmp.isObject())
+            additionalProperties = tmp;
+
+        tmp = node.path("properties");
+
+        if (tmp.isObject())
+            properties.putAll(CollectionUtils.toMap(tmp.fields()));
+
+        tmp = node.path("patternProperties");
+
+        if (tmp.isObject())
+            patternProperties.putAll(CollectionUtils.toMap(tmp.fields()));
     }
 }
---
+++ src/test/java/org/eel/kitchen/jsonschema/schema/SchemaNodeObjectSchemaTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2012, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eel.kitchen.jsonschema.schema;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.eel.kitchen.jsonschema.main.JsonSchemaException;
+import org.eel.kitchen.util.CollectionUtils;
+import org.eel.kitchen.util.JsonLoader;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import static org.testng.Assert.*;
+
+public final class SchemaNodeObjectSchemaTest
+{
+    private JsonNode testData;
+
+    @BeforeClass
+    public void initializeTestData()
+        throws IOException
+    {
+        testData = JsonLoader.fromResource("/schemanode/object.json");
+    }
+
+    @DataProvider
+    private Iterator<Object[]> getData()
+        throws JsonSchemaException
+    {
+        final Set<Object[]> set = new HashSet<Object[]>(testData.size());
+
+        JsonNode schema;
+        Set<JsonNode> expected;
+        String key;
+
+        for (final JsonNode node: testData) {
+            schema = node.get("schema");
+            expected = CollectionUtils.toSet(node.get("expected").elements());
+            key = node.get("key").textValue();
+            set.add(new Object[] { schema, key, expected });
+        }
+
+        return set.iterator();
+    }
+
+    @Test(dataProvider = "getData")
+    public void testArraySchema(final JsonNode schema, final String key,
+        final Set<JsonNode> expected)
+        throws JsonSchemaException
+    {
+        final String errmsg = "object schema lookup failure (schema: "
+            + schema + ", key: " + key + ")";
+        final SchemaContainer container = new SchemaContainer(schema);
+        final SchemaNode node = new SchemaNode(container, schema);
+
+        final Set<JsonNode> actual = node.getObjectSchemas(key);
+
+        assertEquals(actual, expected, errmsg);
+    }
+}
