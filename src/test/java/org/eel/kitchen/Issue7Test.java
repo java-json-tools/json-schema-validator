ad794fbd43c8a5616e71a85a3d5da42d1bfd2e22
Francis Galiegue <fgaliegue@gmail.com>
2012-07-11 14:50:30 +0200

    Fix issue #7, reintroduce performance test
    With all these changes, the context is now properly transferred, which means
    "extends" actually works.

---
+++ src/test/java/org/eel/kitchen/Issue7Test.java
@@ -0,0 +1,49 @@
+package org.eel.kitchen;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.eel.kitchen.jsonschema.main.ValidationContext;
+import org.eel.kitchen.jsonschema.schema.JsonSchema;
+import org.eel.kitchen.jsonschema.schema.JsonSchemaFactory;
+import org.eel.kitchen.util.JsonLoader;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.*;
+
+public final class Issue7Test
+{
+    private JsonNode draftv3, schema1, schema2;
+
+    @BeforeClass
+    public void setUp()
+        throws IOException
+    {
+        draftv3 = JsonLoader.fromResource("/schema-draftv3.json");
+        schema1 = JsonLoader.fromResource("/schema1.json");
+        schema2 = JsonLoader.fromResource("/schema2.json");
+
+    }
+
+    @Test
+    public void testIssue7()
+    {
+        final JsonSchemaFactory factory = new JsonSchemaFactory();
+
+        ValidationContext context;
+        JsonSchema schema;
+
+        context = new ValidationContext();
+        schema = factory.create(draftv3);
+
+        schema.validate(context, schema1);
+        assertTrue(context.isSuccess());
+
+        schema = factory.create(schema1);
+        context = new ValidationContext();
+
+        schema.validate(context, schema2);
+        assertFalse(context.isSuccess());
+    }
+}
