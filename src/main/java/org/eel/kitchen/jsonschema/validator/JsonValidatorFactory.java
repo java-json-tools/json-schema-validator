1bd88f670bf68b4b022f3d3060ddfad7726e257d
Francis Galiegue <fgaliegue@gmail.com>
2012-07-11 12:28:44 +0200

    New JsonValidatorFactory and SimpleJsonValidator classes
    The JsonValidatorFactory uses Guava's LoadingCache and a CacheLoader.
    
    It is intended to be used _after_ $ref is resolved etc.

---
+++ src/main/java/org/eel/kitchen/jsonschema/validator/JsonValidatorFactory.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2012, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eel.kitchen.jsonschema.validator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.eel.kitchen.jsonschema.bundle.KeywordBundle;
+import org.eel.kitchen.jsonschema.keyword.KeywordFactory;
+import org.eel.kitchen.jsonschema.keyword.KeywordValidator;
+import org.eel.kitchen.jsonschema.main.ValidationContext;
+import org.eel.kitchen.jsonschema.syntax.SyntaxValidator;
+
+import java.util.Set;
+
+public final class JsonValidatorFactory
+{
+    private final LoadingCache<JsonNode, JsonValidator> cache;
+
+    public JsonValidatorFactory(final KeywordBundle bundle)
+    {
+        cache = CacheBuilder.newBuilder()
+            .maximumSize(100).build(new ValidatorBuilder(bundle));
+    }
+
+    private static class ValidatorBuilder
+        extends CacheLoader<JsonNode, JsonValidator>
+    {
+        private final SyntaxValidator syntaxValidator;
+        private final KeywordFactory keywordFactory;
+
+        ValidatorBuilder(final KeywordBundle bundle)
+        {
+            syntaxValidator = new SyntaxValidator(bundle);
+            keywordFactory = new KeywordFactory(bundle);
+        }
+
+        @Override
+        public JsonValidator load(final JsonNode key)
+        {
+            final ValidationContext ctx = new ValidationContext();
+            syntaxValidator.validate(ctx, key);
+
+            if (!ctx.isSuccess())
+                return new InvalidJsonValidator(ctx);
+
+            final Set<KeywordValidator> validators
+                = keywordFactory.getValidators(key);
+
+            return new SimpleJsonValidator(validators);
+        }
+    }
+
+    public JsonValidator fromNode(final JsonNode schemaNode)
+    {
+        return cache.getUnchecked(schemaNode);
+    }
+}
---
+++ src/main/java/org/eel/kitchen/jsonschema/validator/SimpleJsonValidator.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2012, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eel.kitchen.jsonschema.validator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.eel.kitchen.jsonschema.keyword.KeywordValidator;
+import org.eel.kitchen.jsonschema.main.ValidationContext;
+
+import java.util.HashSet;
+import java.util.Set;
+
+final class SimpleJsonValidator
+    implements JsonValidator
+{
+    private final Set<KeywordValidator> validators
+        = new HashSet<KeywordValidator>();
+
+    SimpleJsonValidator(final Set<KeywordValidator> validators)
+    {
+        this.validators.addAll(validators);
+    }
+
+    @Override
+    public void validate(final ValidationContext ctx, final JsonNode instance)
+    {
+        for (final KeywordValidator v: validators)
+            v.validateInstance(ctx, instance);
+    }
+}
