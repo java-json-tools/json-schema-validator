6fd0fb5beee7131d0547f9306538709e19341d88
Corey Sciuto <csciuto@constantcontact.com>
2012-08-16 11:31:02 -0400

    Adding myself to pom.xml and fixing another merge issue that I missed the first time.

--- src/main/java/org/eel/kitchen/jsonschema/keyword/FormatKeywordValidator.java
+++ src/main/java/org/eel/kitchen/jsonschema/keyword/FormatKeywordValidator.java
@@ -22,6 +22,7 @@ import com.google.common.collect.ImmutableMap;
 import org.eel.kitchen.jsonschema.ValidationContext;
 import org.eel.kitchen.jsonschema.format.DateFormatSpecifier;
 import org.eel.kitchen.jsonschema.format.DateTimeFormatSpecifier;
+import org.eel.kitchen.jsonschema.format.DateTimeMillisecFormatSpecifier;
 import org.eel.kitchen.jsonschema.format.EmailFormatSpecifier;
 import org.eel.kitchen.jsonschema.format.FormatSpecifier;
 import org.eel.kitchen.jsonschema.format.HostnameFormatSpecifier;
@@ -44,9 +45,14 @@ import java.util.Map;
  * <p>This keyword is scheduled for disappearance in draft v4. However,
  * some people have raised concerns about this.</p>
  *
- * <p>All format specifiers fom draft v3 are supported except {@code style}
+ * <p>All format specifiers from draft v3 are supported except {@code style}
  * and {@code color} (which validate an entire CSS 2.1 style and color
  * respectively!).</p>
+ * 
+ * <p>There is support here for one custom specifier: {@code date-time-ms}. The v3 draft
+ * specifies that {@code date-time} should match the pattern {@code YYYY-MM-DDThh:mm:ssZ}.
+ * {@code date-time-ms} extends that to a format supporting milliseconds: 
+ * {@code YYYY-MM-DDThh:mm:ss.SSSZ}.</p>
  *
  * @see org.eel.kitchen.jsonschema.format
  */
