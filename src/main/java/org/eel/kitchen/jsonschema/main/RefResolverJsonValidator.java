300534492538c52a7a460c77eeddb80bf4da4b86
Francis Galiegue <fgaliegue@gmail.com>
2012-08-20 14:00:01 +0200

    RefResolverJsonValidator: rewrite so as to validate in a single instance
    We don't gain much in returning "this" over and over again. And the code is also
    cleaner as a bonus.
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/org/eel/kitchen/jsonschema/main/RefResolverJsonValidator.java
+++ src/main/java/org/eel/kitchen/jsonschema/main/RefResolverJsonValidator.java
@@ -34,7 +34,6 @@ public final class RefResolverJsonValidator
     private final Set<JsonRef> refs = new LinkedHashSet<JsonRef>();
 
     private SchemaNode schemaNode;
-    private boolean isRef;
 
     public RefResolverJsonValidator(final JsonSchemaFactory factory,
         final SchemaNode schemaNode)
@@ -49,54 +48,55 @@ public final class RefResolverJsonValidator
         final ValidationReport report, final JsonNode instance)
     {
         JsonNode node = schemaNode.getNode();
-        final JsonNode refNode = node.path("$ref");
-        isRef = JacksonUtils.nodeIsURI(refNode);
 
-        if (!isRef)
-            return true;
-
-        SchemaContainer container = schemaNode.getContainer();
-
-        final JsonRef origin = container.getLocator();
-        JsonRef ref;
-        try {
-            ref = JsonRef.fromString(refNode.textValue());
-        } catch (JsonSchemaException e) {
-            // should not happen
-            throw new RuntimeException("WTF??", e);
-        }
-
-        ref = origin.resolve(ref);
-
-        if (!origin.contains(ref))
+        while (JacksonUtils.nodeIsURI(node.path("$ref")))
             try {
-                container = registry.get(ref.getRootAsURI());
-                context.setContainer(container);
+                node = resolve(context, node);
             } catch (JsonSchemaException e) {
-                report.addMessage("$ref resolution error: " + e.getMessage());
+                report.addMessage(e.getMessage());
                 return false;
             }
 
-        node = ref.getFragment().resolve(container.getSchema());
-
-        if (node.isMissingNode()) {
-            report.addMessage("$ref resolution error: dangling JSON reference "
-                + ref);
-            return false;
-        }
-
-        if (!refs.add(ref)) {
-            report.addMessage("$ref resolution error: loop detected: " + refs);
-            return false;
-        }
-
-        schemaNode = new SchemaNode(container, node);
+        schemaNode = new SchemaNode(context.getContainer(), node);
         return true;
     }
 
     @Override
     public JsonValidator next()
     {
-        return isRef ? this : new SyntaxJsonValidator(factory, schemaNode);
+        return new SyntaxJsonValidator(factory, schemaNode);
+    }
+
+    private JsonNode resolve(final ValidationContext context,
+        final JsonNode node)
+        throws JsonSchemaException
+    {
+        SchemaContainer container = context.getContainer();
+
+        final JsonRef source = container.getLocator();
+        final JsonRef ref = JsonRef.fromString(node.get("$ref").textValue());
+        final JsonRef target = source.resolve(ref);
+
+        if (!refs.add(target))
+            throw new JsonSchemaException("$ref problem: ref loop detected: "
+                + refs);
+
+        if (!source.contains(target)) {
+            container = registry.get(target.getRootAsURI());
+            context.setContainer(container);
+        }
+
+        final JsonNode ret
+            = target.getFragment().resolve(container.getSchema());
+
+        if (ret.isMissingNode())
+            throw new JsonSchemaException("$ref problem: dangling JSON pointer"
+                + target);
+
+        if (!ret.isObject())
+            throw new JsonSchemaException("$ref problem: JSON document is not" +
+                " a schema (not an object)");
+
+        return ret;
     }
 }
