d81f392a54b4c9949f4b9a61fa36da166aa96322
Francis Galiegue <fgaliegue@gmail.com>
2012-09-04 18:47:24 +0200

    Fix bug in array element schema calculation when "items" is missing
    If "items" is missing, it is equivalent to an empty schema.
    
    This means that with this schema:
    
    {
        "additionalItems": { "type": "string" }
    }
    
    any element of an array instance would have to obey the empty schema ({}), NOT
    the schema in "additionalItems".
    
    Add tests. And use Guava's @VisibleForTesting annotation while we are at it
    (again another useful tool -- Guava really rocks).
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

---
+++ src/test/java/org/eel/kitchen/jsonschema/validator/ArrayValidatorTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2012, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eel.kitchen.jsonschema.validator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.eel.kitchen.jsonschema.util.JacksonUtils;
+import org.eel.kitchen.jsonschema.util.JsonLoader;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.internal.annotations.Sets;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+/*
+ * TODO:
+ *
+ * - more tests,
+ * - do that for object nodes too
+ */
+public final class ArrayValidatorTest
+{
+    private JsonNode testData;
+
+    @BeforeClass
+    public void initData()
+        throws IOException
+    {
+        testData = JsonLoader.fromResource("/validator/array.json");
+    }
+
+    @DataProvider
+    public Iterator<Object[]> getData()
+    {
+        final Set<Object[]> set = Sets.newHashSet();
+
+        for (final JsonNode node: testData)
+            set.add(new Object[] {
+                node.get("schema"),
+                node.get("index").intValue(),
+                node.get("expected")
+            });
+
+        return set.iterator();
+    }
+
+    @Test(dataProvider = "getData")
+    public void arrayElementSchemasAreCorrectlyComputed(final JsonNode schema,
+        final int index, final JsonNode expected)
+    {
+        final ArrayValidator validator = new ArrayValidator(schema);
+
+        // This is ugly, I know... But ObjectNode's .equals() basically forbids
+        // inheritance, so I have to resort to that, since JacksonUtils'
+        // .emptyMap() is NOT an ObjectNode :(
+        final Map<String, JsonNode> expectedMap
+            = JacksonUtils.nodeToMap(expected);
+        final Map<String, JsonNode> actualMap
+            = JacksonUtils.nodeToMap(validator.getSchema(index));
+
+        assertEquals(expectedMap, actualMap);
+    }
+}
