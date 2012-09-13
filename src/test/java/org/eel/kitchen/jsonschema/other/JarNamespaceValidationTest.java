923c7a7ef5abf79294c62e1c3d146d2c8a552336
Francis Galiegue <fgaliegue@gmail.com>
2012-09-13 16:53:57 +0200

    Add skeleton test file for opaque namespace testing
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

---
+++ src/test/java/org/eel/kitchen/jsonschema/other/JarNamespaceValidationTest.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2012, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.eel.kitchen.jsonschema.other;
+
+import com.google.common.io.Files;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import static org.testng.Assert.*;
+
+public final class JarNamespaceValidationTest
+{
+    private URI rootURI;
+    private File jarLocation;
+
+
+    @BeforeClass
+    public void buildJar()
+        throws IOException, URISyntaxException
+    {
+        rootURI = getClass().getResource("/").toURI();
+        jarLocation = doBuildJar();
+    }
+
+    @Test
+    public void foo()
+    {
+        assertTrue(true);
+    }
+
+    @AfterClass
+    public void deleteJar()
+        throws IOException
+    {
+        if (!jarLocation.delete())
+            throw new IOException("Could not delete " + jarLocation);
+    }
+
+    private File doBuildJar()
+        throws IOException, URISyntaxException
+    {
+        final File tmpdir = new File(System.getProperty("java.io.tmpdir"));
+        final File ret = new File(tmpdir, UUID.randomUUID().toString()
+            + ".jar").getCanonicalFile();
+
+        final URI baseURI = getClass().getResource("/grimbo").toURI();
+
+        final FileOutputStream fh = new FileOutputStream(ret);
+        final JarOutputStream jarfh = new JarOutputStream(fh);
+
+        try {
+            doWriteJar(jarfh, baseURI);
+        } finally {
+            fh.flush();
+            jarfh.flush();
+
+            jarfh.close();
+            fh.close();
+        }
+
+        return ret;
+    }
+
+    private void doWriteJar(final JarOutputStream jarfh, final URI baseURI)
+        throws IOException
+    {
+        final File src = new File(baseURI.getPath());
+        final String basePath = "/" + rootURI.relativize(baseURI)
+            + (src.isDirectory() ? "/" : "");
+
+        final JarEntry entry = new JarEntry(basePath);
+        jarfh.putNextEntry(entry);
+
+        if (src.isDirectory()) {
+            for (final File file: src.listFiles())
+                doWriteJar(jarfh, file.toURI());
+            return;
+        }
+
+        Files.copy(src, jarfh);
+    }
+}
