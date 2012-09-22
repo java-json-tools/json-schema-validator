64e8229b086f987e89529d57504b5eaf5b260124
Francis Galiegue <fgaliegue@gmail.com>
2012-09-22 16:18:47 +0200

    Rename JsonFragmentTest
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/test/java/org/eel/kitchen/jsonschema/ref/JsonPointerLookupTest.java
+++ src/test/java/org/eel/kitchen/jsonschema/ref/JsonPointerLookupTest.java
@@ -18,7 +18,6 @@
 package org.eel.kitchen.jsonschema.ref;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.MissingNode;
 import com.google.common.collect.ImmutableSet;
 import org.eel.kitchen.jsonschema.util.JsonLoader;
 import org.testng.annotations.BeforeClass;
@@ -30,7 +29,7 @@ import java.util.Iterator;
 
 import static org.testng.Assert.*;
 
-public final class JsonFragmentTest
+public final class JsonPointerLookupTest
 {
     private JsonNode schema;
 
@@ -56,7 +55,7 @@ public final class JsonFragmentTest
         expected = schema;
 
         input = "";
-        errmsg = "empty fragment does not resolve to self";
+        errmsg = "empty pointer does not resolve to self";
         builder.add(new Object[] { input, expected, errmsg });
 
         /*
@@ -73,18 +72,12 @@ public final class JsonFragmentTest
             .get("type").get(1);
         builder.add(new Object[] { input, expected, errmsg });
 
-        errmsg = "non existing JSON Pointer lookup failed";
-        expected = MissingNode.getInstance();
-
-        input = "/foobar"; // an ID exists, but not a pointer
-        builder.add(new Object[] { input, expected, errmsg });
-
         return builder.build().iterator();
     }
 
     @Test(dataProvider = "getData")
-    public void fragmentLookup(final String input, final JsonNode expected,
-        final String errmsg)
+    public void pointerLookupSucceed(final String input,
+        final JsonNode expected, final String errmsg)
     {
         final JsonFragment fragment = JsonFragment.fromFragment(input);
 
