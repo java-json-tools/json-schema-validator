1f4e6052976108eb179fd737a88ae2111736f691
Francis Galiegue <fgaliegue@gmail.com>
2012-11-08 23:49:01 +0100

    Syntax, draft v4: add maxProperties check
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/org/eel/kitchen/jsonschema/metaschema/SyntaxCheckers.java
+++ src/main/java/org/eel/kitchen/jsonschema/metaschema/SyntaxCheckers.java
@@ -198,6 +198,9 @@ public final class SyntaxCheckers
         checker = new PositiveIntegerSyntaxChecker(keyword);
         draftv4.put(keyword, checker);
 
+        keyword = "maxProperties";
+        checker = new PositiveIntegerSyntaxChecker(keyword);
+        draftv4.put(keyword, checker);
         // Build the map: all checkers in common, plus draft v4 specific
         // checkers
         draftv4.putAll(commonCheckers);
