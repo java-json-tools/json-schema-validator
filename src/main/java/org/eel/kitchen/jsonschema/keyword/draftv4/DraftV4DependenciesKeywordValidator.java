2d6eebc7e9744649d4e04e591255fe74b2f5b8a7
Francis Galiegue <fgaliegue@gmail.com>
2012-12-17 14:18:41 +0100

    Catch up with draft v4 dependencies validation
    Array of strings for property dependency has been reintroduced, account for it.
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/org/eel/kitchen/jsonschema/keyword/draftv4/DraftV4DependenciesKeywordValidator.java
+++ src/main/java/org/eel/kitchen/jsonschema/keyword/draftv4/DraftV4DependenciesKeywordValidator.java
@@ -18,7 +18,8 @@
 package org.eel.kitchen.jsonschema.keyword.draftv4;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
 import org.eel.kitchen.jsonschema.keyword.KeywordValidator;
 import org.eel.kitchen.jsonschema.report.Message;
@@ -40,31 +41,57 @@ import java.util.Set;
 public final class DraftV4DependenciesKeywordValidator
     extends KeywordValidator
 {
-    private final Map<String, JsonNode> dependencies;
+    private final Map<String, JsonNode> schemaDeps = Maps.newHashMap();
+    private final Map<String, Set<String>> propertyDeps = Maps.newHashMap();
 
     public DraftV4DependenciesKeywordValidator(final JsonNode schema)
     {
         super("dependencies", NodeType.OBJECT);
-        dependencies = ImmutableMap.copyOf(
-            JacksonUtils.nodeToMap(schema.get(keyword))
-        );
+
+        final Map<String, JsonNode> map
+            = JacksonUtils.nodeToMap(schema.get(keyword));
+
+        String key;
+        JsonNode value;
+
+        for (final Map.Entry<String, JsonNode> entry: map.entrySet()) {
+            key = entry.getKey();
+            value = entry.getValue();
+            if (value.isObject()) { // Schema dependency
+                schemaDeps.put(key, value);
+                continue;
+            }
+            final Set<String> set = Sets.newHashSet();
+            for (final JsonNode node: value)
+                set.add(node.textValue());
+            propertyDeps.put(key, set);
+        }
     }
 
     @Override
     protected void validate(final ValidationContext context,
         final ValidationReport report, final JsonNode instance)
     {
+        if (!schemaDeps.isEmpty())
+            processSchemaDeps(context, report, instance);
+        if (!propertyDeps.isEmpty())
+            processPropertyDeps(report, instance);
+    }
+
+    private void processSchemaDeps(final ValidationContext context,
+        final ValidationReport report, final JsonNode instance)
+    {
         final Set<String> fields = Sets.newHashSet(instance.fieldNames());
         final Set<String> unsatisfied = Sets.newTreeSet();
 
-        fields.retainAll(dependencies.keySet());
+        fields.retainAll(schemaDeps.keySet());
 
         JsonNode subSchema;
         JsonValidator validator;
         ValidationReport subReport;
 
         for (final String field: fields) {
-            subSchema = dependencies.get(field);
+            subSchema = schemaDeps.get(field);
             validator = context.newValidator(subSchema);
             subReport = report.copy();
             validator.validate(context, subReport, instance);
@@ -82,9 +109,33 @@ public final class DraftV4DependenciesKeywordValidator
         report.addMessage(msg.build());
     }
 
+    private void processPropertyDeps(final ValidationReport report,
+        final JsonNode instance)
+    {
+        final Set<String> fields = Sets.newHashSet(instance.fieldNames());
+
+        Set<String> deps, missing;
+
+        for (final String field: fields) {
+            deps = propertyDeps.get(field);
+            if (deps == null)
+                continue;
+            missing = Sets.newHashSet(deps);
+            missing.removeAll(fields);
+            if (missing.isEmpty())
+                continue;
+            report.addMessage(newMsg().addInfo("property", field)
+                .setMessage("unsatisfied property dependencies")
+                .addInfo("required", Ordering.natural().sortedCopy(deps))
+                .addInfo("missing", Ordering.natural().sortedCopy(missing))
+                .build());
+        }
+    }
+
     @Override
     public String toString()
     {
-        return "schema dependencies on properties: " + dependencies.keySet();
+        return "schema dependencies on properties: " + schemaDeps.keySet()
+            + "; property dependencies on properties: " + propertyDeps.keySet();
     }
 }
