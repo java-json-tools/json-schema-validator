java-json-tools/json-schema-validator

commit 7f83965011e217ed91008396e016e7f662fba19c

JacskonUtils riddance, step 2: remove .emptySchema()
Create a static .emptyObject() method in CustomJsonNodeFactory instead.

Also replace a reference comparison with an appropriate test for size.

Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

src/main/java/org/eel/kitchen/jsonschema/validator/JsonValidatorCache.java
@@ -33,7 +33,6 @@ import org.eel.kitchen.jsonschema.report.ValidationReport;
 import org.eel.kitchen.jsonschema.schema.SchemaNode;
 import org.eel.kitchen.jsonschema.schema.SchemaRegistry;
 import org.eel.kitchen.jsonschema.syntax.SyntaxValidator;
-import org.eel.kitchen.jsonschema.util.jackson.JacksonUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -118,7 +117,7 @@ public final class JsonValidatorCache
             {
                 // We can do that: we ask JacksonUtils to return an empty schema
                 // each time we have to return one.
-                if (key.getNode() == JacksonUtils.emptySchema())
+                if (key.getNode().size() == 0)
                     return ALWAYS_TRUE;
 
                 final SchemaNode realNode;

src/main/java/org/eel/kitchen/jsonschema/util/jackson/CustomJsonNodeFactory.java
@@ -21,6 +21,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.NumericNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.ValueNode;
 
 import java.math.BigDecimal;
@@ -68,6 +69,8 @@ public final class CustomJsonNodeFactory
         .enable(DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS)
         .setNodeFactory(INSTANCE);
 
+    private static final ObjectNode EMPTY_OBJECT = INSTANCE.objectNode();
+
     /**
      * Get the only instance of the class
      *
@@ -86,6 +89,18 @@ public final class CustomJsonNodeFactory
     public static ObjectMapper getMapper()
     {
         return MAPPER;
+    }
+
+    /**
+     * Return a statically created empty object
+     *
+     * <p>WARNING: this instance is mutable, but please don't do that!</p>
+     *
+     * @return an {@link ObjectNode}
+     */
+    public static ObjectNode emptyObject()
+    {
+        return EMPTY_OBJECT;
     }
 
     private CustomJsonNodeFactory()

src/main/java/org/eel/kitchen/jsonschema/util/jackson/JacksonUtils.java
@@ -29,9 +29,6 @@ import java.util.Map;
 
 public final class JacksonUtils
 {
-    private static final JsonNode EMPTY_SCHEMA
-        = CustomJsonNodeFactory.getInstance().objectNode();
-
     private JacksonUtils()
     {
     }
@@ -55,15 +52,5 @@ public final class JacksonUtils
         }
 
         return ret;
-    }
-
-    /**
-     * Return an empty schema
-     *
-     * @return a statically created, empty, JSON object.
-     */
-    public static JsonNode emptySchema()
-    {
-        return EMPTY_SCHEMA;
     }
 }

src/main/java/org/eel/kitchen/jsonschema/validator/ArrayValidator.java
@@ -22,7 +22,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import org.eel.kitchen.jsonschema.ref.JsonPointer;
 import org.eel.kitchen.jsonschema.report.ValidationReport;
-import org.eel.kitchen.jsonschema.util.jackson.JacksonUtils;
+import org.eel.kitchen.jsonschema.util.jackson.CustomJsonNodeFactory;
 
 import java.util.Collections;
 import java.util.List;
@@ -61,7 +61,7 @@ final class ArrayValidator
         if (!node.isArray()) {
             // Either it is missing or it is an object
             additionalItems = node.isObject() ? node
-                : JacksonUtils.emptySchema();
+                : CustomJsonNodeFactory.emptyObject();
             items = Collections.emptyList();
             return;
         }
@@ -70,7 +70,8 @@ final class ArrayValidator
 
         node = schema.path("additionalItems");
 
-        additionalItems = node.isObject() ? node : JacksonUtils.emptySchema();
+        additionalItems = node.isObject() ? node
+            :CustomJsonNodeFactory.emptyObject();
     }
 
     @Override

src/main/java/org/eel/kitchen/jsonschema/validator/ObjectValidator.java
@@ -23,6 +23,7 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import org.eel.kitchen.jsonschema.ref.JsonPointer;
 import org.eel.kitchen.jsonschema.report.ValidationReport;
+import org.eel.kitchen.jsonschema.util.jackson.CustomJsonNodeFactory;
 import org.eel.kitchen.jsonschema.util.jackson.JacksonUtils;
 import org.eel.kitchen.jsonschema.util.RhinoHelper;
 
@@ -61,7 +62,7 @@ final class ObjectValidator
 
         node = schema.path("additionalProperties");
         additionalProperties = node.isObject() ? node
-            : JacksonUtils.emptySchema();
+            : CustomJsonNodeFactory.emptyObject();
 
         node = schema.path("properties");
         properties = node.isObject() ? JacksonUtils.nodeToMap(node)

commit 05f4f538f6ced93d1f45f2f6c48dc27bdefae591

Fix critical bug in JsonValidatorCache
Commit 7f83965011e217ed91008396e016e7f662fba19c introduced a fatal bug: we only
checked for the node size before we decided to return a validator which is
always true.

But all value nodes have size 0! As such, all value nodes would be accounted for
as schemas which would validate every instance successfully.

Not good.

Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

src/main/java/org/eel/kitchen/jsonschema/validator/JsonValidatorCache.java
@@ -34,6 +34,7 @@ import org.eel.kitchen.jsonschema.report.ValidationReport;
 import org.eel.kitchen.jsonschema.schema.SchemaNode;
 import org.eel.kitchen.jsonschema.schema.SchemaRegistry;
 import org.eel.kitchen.jsonschema.syntax.SyntaxValidator;
+import org.eel.kitchen.jsonschema.util.jackson.JacksonUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -127,7 +128,7 @@ public final class JsonValidatorCache
             @Override
             public JsonValidator load(final SchemaNode key)
             {
-                if (key.getNode().size() == 0)
+                if (key.getNode().equals(JacksonUtils.emptyObject()))
                     return ALWAYS_TRUE;
 
                 final SchemaNode realNode;