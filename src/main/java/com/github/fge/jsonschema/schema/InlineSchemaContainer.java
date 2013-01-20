07bd97e30040cc33bff06453f371822ba4e643f2
Francis Galiegue <fgaliegue@gmail.com>
2013-01-20 18:46:59 +0100

    InlineSchemaContainer: fix bug with duplicate ids
    Use a Map instead of an ImmutableMap.Builder to collect values (but still store
    the final result in an ImmutableMap).
    
    This means that if there are duplicate ids, the result is undefined, of
    course...
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/com/github/fge/jsonschema/schema/InlineSchemaContainer.java
+++ src/main/java/com/github/fge/jsonschema/schema/InlineSchemaContainer.java
@@ -18,9 +18,10 @@
 package com.github.fge.jsonschema.schema;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.collect.ImmutableMap;
 import com.github.fge.jsonschema.main.JsonSchemaException;
 import com.github.fge.jsonschema.ref.JsonRef;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
 
 import java.util.Map;
 
@@ -29,6 +30,9 @@ import java.util.Map;
  *
  * <p>This container will retrieve all {@code id} members in the schema
  * (including subschemas).</p>
+ *
+ * <p><b>NOTE:</b> if a duplicate {@code id} value is found within the schema,
+ * the actual matching schema is <b>UNDEFINED</b>.</p>
  */
 public final class InlineSchemaContainer
     extends SchemaContainer
@@ -38,12 +42,11 @@ public final class InlineSchemaContainer
     InlineSchemaContainer(final JsonNode schema)
     {
         super(extractLocator(schema).toURI(), schema);
-        final ImmutableMap.Builder<JsonRef, JsonNode> builder
-            = ImmutableMap.builder();
+        final Map<JsonRef, JsonNode> map = Maps.newHashMap();
 
-        builder.put(locator, this.schema);
-        fillURIMap(locator, this.schema, builder);
-        schemas = builder.build();
+        map.put(locator, this.schema);
+        fillURIMap(locator, this.schema, map);
+        schemas = ImmutableMap.copyOf(map);
     }
 
     @Override
@@ -69,7 +72,7 @@ public final class InlineSchemaContainer
     }
 
     private static void fillURIMap(final JsonRef baseRef, final JsonNode node,
-        final ImmutableMap.Builder<JsonRef, JsonNode> builder)
+        final Map<JsonRef, JsonNode> map)
     {
         // Do nothing if the node is not an object
         if (!node.isObject())
@@ -82,11 +85,11 @@ public final class InlineSchemaContainer
                 try {
                     idRef = refFromNode(child.get("id"));
                     resolvedRef = baseRef.resolve(idRef);
-                    builder.put(resolvedRef, cleanup(child));
+                    map.put(resolvedRef, cleanup(child));
                 } catch (JsonSchemaException ignored) {
                     // Do nothing
                 }
-            fillURIMap(baseRef, cleanup(child), builder);
+            fillURIMap(baseRef, cleanup(child), map);
         }
     }
 
