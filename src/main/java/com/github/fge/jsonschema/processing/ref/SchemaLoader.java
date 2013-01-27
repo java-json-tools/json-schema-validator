d944b5fc8d8f9b007a78efce523e5a1b69e51628
Francis Galiegue <fgaliegue@gmail.com>
2013-01-28 00:26:48 +0100

    Port SchemaRegistry to the new architecture (as SchemaLoader)
    Also port tests. But those need to be rewritten.
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

---
+++ src/main/java/com/github/fge/jsonschema/processing/ref/SchemaLoader.java
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.fge.jsonschema.processing.ref;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jsonschema.processing.ProcessingException;
+import com.github.fge.jsonschema.processing.ProcessingMessage;
+import com.github.fge.jsonschema.ref.JsonRef;
+import com.github.fge.jsonschema.schema.SchemaBundle;
+import com.github.fge.jsonschema.tree.CanonicalSchemaTree;
+import com.github.fge.jsonschema.tree.InlineSchemaTree;
+import com.github.fge.jsonschema.tree.JsonSchemaTree;
+import com.google.common.base.Preconditions;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.net.URI;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * A JSON Schema registry
+ *
+ * <p>All schema registering and downloading is done through this class.</p>
+ *
+ * <p>Note that if the id of a schema is not absolute (that is, the URI itself
+ * is absolute and it has no fragment part, or an empty fragment), then the
+ * whole schema will be considered anonymous.</p>
+ *
+ * <p>This class is thread safe.</p>
+ */
+public final class SchemaLoader
+{
+    /**
+     * The default namespace
+     */
+    private final JsonRef namespace;
+
+    /**
+     * Schema cache
+     */
+    private final LoadingCache<URI, JsonNode> cache;
+
+    /**
+     * Whether we want inline dereferencing
+     */
+    private final boolean inline;
+
+    /**
+     * Constructor
+     *
+     * @param manager the URI manager to use
+     * @param namespace this registry's namespace
+     * @param inline whether to use inline dereferencing
+     */
+    public SchemaLoader(final URIManager manager, final URI namespace,
+        final boolean inline)
+    {
+        this.inline = inline;
+        this.namespace = JsonRef.fromURI(namespace);
+        cache = CacheBuilder.newBuilder().maximumSize(100L)
+            .build(new CacheLoader<URI, JsonNode>()
+            {
+                @Override
+                public JsonNode load(final URI key)
+                    throws ProcessingException
+                {
+                    return manager.getContent(key);
+                }
+            });
+    }
+
+    public JsonSchemaTree load(final JsonNode schema)
+    {
+        Preconditions.checkNotNull(schema, "cannot register null schema");
+
+        return inline
+            ? new InlineSchemaTree(schema)
+            : new CanonicalSchemaTree(schema);
+    }
+
+    /**
+     * Get a schema tree from the given URI
+     *
+     * <p>Note that if the URI is relative, it will be resolved against this
+     * registry's namespace, if any.</p>
+     *
+     * @param uri the URI
+     * @return a schema tree
+     */
+    public JsonSchemaTree get(final URI uri)
+        throws ProcessingException
+    {
+        final JsonRef ref = namespace.resolve(JsonRef.fromURI(uri));
+
+        final ProcessingMessage msg = new ProcessingMessage()
+            .put("uri", ref);
+
+        if (!ref.isAbsolute())
+            throw new ProcessingException(msg.msg("URI is not absolute"));
+
+        final URI realURI = ref.toURI();
+
+        try {
+            final JsonNode node = cache.get(realURI);
+            return inline
+                ? new InlineSchemaTree(ref, node)
+                : new CanonicalSchemaTree(ref, node);
+        } catch (ExecutionException e) {
+            throw (ProcessingException) e.getCause();
+        }
+    }
+
+    /**
+     * Load a schema bundle into this registry
+     *
+     * <p>As a schema bundle is guaranteed to have well-formed locators, schemas
+     * from a bundle can be directly injected into the cache.</p>
+     */
+    public void addBundle(final SchemaBundle bundle)
+    {
+        cache.putAll(bundle.getSchemas());
+    }
+
+    @Override
+    public String toString()
+    {
+        return cache.stats().toString();
+    }
+}
---
+++ src/test/java/com/github/fge/jsonschema/processing/ref/SchemaLoaderTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.fge.jsonschema.processing.ref;
+
+import com.github.fge.jsonschema.processing.LogThreshold;
+import com.github.fge.jsonschema.processing.ProcessingException;
+import com.github.fge.jsonschema.processing.ProcessingMessage;
+import com.github.fge.jsonschema.ref.JsonRef;
+import com.github.fge.jsonschema.schema.SchemaBundle;
+import com.github.fge.jsonschema.tree.JsonSchemaTree;
+import com.github.fge.jsonschema.uri.URIDownloader;
+import com.github.fge.jsonschema.util.jackson.JacksonUtils;
+import org.testng.annotations.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+import static org.mockito.Mockito.*;
+import static org.testng.Assert.*;
+
+public final class SchemaLoaderTest
+{
+    private static final byte[] BYTES = JacksonUtils.nodeFactory().objectNode()
+        .toString().getBytes();
+    @Test
+    public void namespacesAreRespected()
+        throws ProcessingException, IOException
+    {
+        final URI fullPath = URI.create("foo:/baz#");
+        final URIManager manager = new URIManager();
+        final URIDownloader downloader = spy(new URIDownloader()
+        {
+            @Override
+            public InputStream fetch(final URI source)
+                throws IOException
+            {
+                if (!fullPath.equals(source))
+                    throw new IOException();
+                return new ByteArrayInputStream(BYTES);
+            }
+        });
+
+        manager.registerScheme("foo", downloader);
+
+        final URI rootns = URI.create("foo:///bar/../bar/");
+
+        final SchemaLoader loader = new SchemaLoader(manager, rootns, false);
+
+        final URI uri = URI.create("../baz");
+        loader.get(uri);
+        final JsonRef ref = JsonRef.fromURI(rootns.resolve(uri));
+        verify(downloader).fetch(rootns.resolve(ref.toURI()));
+    }
+
+    @Test
+    public void URIsAreNormalizedBehindTheScenes()
+        throws ProcessingException
+    {
+        final SchemaBundle bundle = new SchemaBundle();
+        final String location = "http://toto/a/../b";
+
+        bundle.addSchema(location, JacksonUtils.nodeFactory().objectNode());
+
+        final SchemaLoader loader = new SchemaLoader(new URIManager(),
+            URI.create("#"), false);
+
+        loader.addBundle(bundle);
+
+        final JsonSchemaTree tree = loader.get(URI.create(location));
+
+        assertEquals(tree.getLoadingRef().toURI(),
+            URI.create("http://toto/b#"));
+    }
+
+    @Test
+    public void NonAbsoluteURIsAreRefused()
+    {
+        final SchemaLoader loader = new SchemaLoader(new URIManager(),
+            URI.create("#"), false);
+
+        final URI target = URI.create("moo#");
+
+        final ProcessingMessage msg = new ProcessingMessage()
+            .setLogThreshold(LogThreshold.FATAL).put("uri", target)
+            .msg("URI is not absolute");
+
+        try {
+            loader.get(target);
+            fail("No exception thrown!");
+        } catch (ProcessingException e) {
+            assertEquals(e.getProcessingMessage(), msg);
+        }
+    }
+
+    @Test
+    public void injectedSchemasAreNotFetchedAgain()
+        throws ProcessingException, IOException
+    {
+        final URI uri = URI.create("http://foo.bar/baz#");
+        final SchemaBundle bundle = new SchemaBundle();
+
+        bundle.addSchema(uri, JacksonUtils.nodeFactory().objectNode());
+
+        final URIDownloader mock = mock(URIDownloader.class);
+        final URIManager manager = new URIManager();
+        manager.registerScheme("http", mock);
+
+        final SchemaLoader registry = new SchemaLoader(manager, URI.create("#"),
+            false);
+        registry.addBundle(bundle);
+
+        registry.get(uri);
+        verify(mock, never()).fetch(uri);
+    }
+
+    @Test
+    public void schemasAreFetchedOnceNotTwice()
+        throws ProcessingException, IOException
+    {
+        final URI uri = URI.create("foo:/baz#");
+        final URIManager manager = new URIManager();
+        final URIDownloader downloader = spy(new URIDownloader()
+        {
+            @Override
+            public InputStream fetch(final URI source)
+                throws IOException
+            {
+                return new ByteArrayInputStream(BYTES);
+            }
+        });
+
+        manager.registerScheme("foo", downloader);
+        final SchemaLoader loader = new SchemaLoader(manager, URI.create("#"),
+            false);
+
+        loader.get(uri);
+        loader.get(uri);
+        verify(downloader, times(1)).fetch(uri);
+    }
+}
