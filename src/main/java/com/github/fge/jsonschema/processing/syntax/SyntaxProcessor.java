f06f0604eeb80368858a4a4af6715579567bd851
Francis Galiegue <fgaliegue@gmail.com>
2013-01-30 04:26:40 +0100

    SyntaxProcessor: quite a few code changes again
    Slowly getting there. Too slowly, I might add.
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/com/github/fge/jsonschema/processing/syntax/SyntaxProcessor.java
+++ src/main/java/com/github/fge/jsonschema/processing/syntax/SyntaxProcessor.java
@@ -23,6 +23,9 @@ import com.github.fge.jsonschema.processing.ValidationData;
 import com.github.fge.jsonschema.report.ProcessingReport;
 import com.github.fge.jsonschema.tree.JsonSchemaTree;
 import com.google.common.base.Equivalence;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.Maps;
 
 import java.util.Map;
@@ -30,8 +33,42 @@ import java.util.Map;
 public final class SyntaxProcessor
     implements Processor<ValidationData, ValidationData>
 {
+    /**
+     * Equivalence specifically defined for syntax checking
+     *
+     * <p>By default, {@link JsonSchemaTree}'s equality is based on the loading
+     * JSON Reference and schema. But for syntax checking we need to compare the
+     * schema with the current location in it, so that we can accurately report
+     * non visited paths and look up these as keys.</p>
+     */
+    private static final Equivalence<JsonSchemaTree> EQUIVALENCE
+        = new Equivalence<JsonSchemaTree>()
+    {
+        @Override
+        protected boolean doEquivalent(final JsonSchemaTree a,
+            final JsonSchemaTree b)
+        {
+            return a.getCurrentRef().equals(b.getCurrentRef())
+                && a.getBaseNode().equals(b.getBaseNode());
+        }
+
+        @Override
+        protected int doHash(final JsonSchemaTree t)
+        {
+            return 31 * t.getCurrentRef().hashCode()
+                + t.getBaseNode().hashCode();
+        }
+    };
+
     private final Map<String, SyntaxChecker> checkers = Maps.newTreeMap();
 
+    private final LoadingCache<Equivalence.Wrapper<JsonSchemaTree>,
+        SyntaxValidationResults> cache;
+
+    public SyntaxProcessor()
+    {
+        cache = CacheBuilder.newBuilder().build(loader());
+    }
     /**
      * Process the input
      *
@@ -48,30 +85,18 @@ public final class SyntaxProcessor
         return input;
     }
 
-    /**
-     * Equivalence class specifically defined for syntax checking
-     *
-     * <p>By default, {@link JsonSchemaTree}'s equality is based on the loading
-     * JSON Reference and schema. But for syntax checking we need to compare the
-     * schema with the current location in it, so that we can accurately report
-     * non visited paths and look up these as keys.</p>
-     */
-    private static final class SyntaxEquivalence
-        extends Equivalence<JsonSchemaTree>
+    private CacheLoader<Equivalence.Wrapper<JsonSchemaTree>,
+        SyntaxValidationResults> loader()
     {
-        @Override
-        protected boolean doEquivalent(final JsonSchemaTree a,
-            final JsonSchemaTree b)
+        return new CacheLoader<Equivalence.Wrapper<JsonSchemaTree>, SyntaxValidationResults>()
         {
-            return a.getCurrentRef().equals(b.getCurrentRef())
-                && a.getBaseNode().equals(b.getBaseNode());
-        }
-
-        @Override
-        protected int doHash(final JsonSchemaTree t)
-        {
-            return 31 * t.getCurrentRef().hashCode() + t.getBaseNode().hashCode();
-        }
-
+            @Override
+            public SyntaxValidationResults load(
+                final Equivalence.Wrapper<JsonSchemaTree> key)
+                throws ProcessingException
+            {
+                return new SyntaxValidationResults();
+            }
+        };
     }
 }
