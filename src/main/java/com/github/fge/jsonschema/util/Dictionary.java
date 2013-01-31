dbf4f84dd78f3600ad2acfb1d9eb77cddcda1001
Francis Galiegue <fgaliegue@gmail.com>
2013-01-31 02:50:03 +0100

    New utility class: Dictionary
    Since keywords are strings and have a s*load of associated objects, create a
    utility class to hold them, with often-used methods.
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

---
+++ src/main/java/com/github/fge/jsonschema/util/Dictionary.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.fge.jsonschema.util;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/*
+ * Wrapper class over a Map<String, T>
+ *
+ * Use it to collect what exists as strings, entries etc.
+ */
+public final class Dictionary<T>
+{
+    private final Map<String, T> entries = Maps.newHashMap();
+
+    public Set<String> missingEntriesFrom(final Set<String> set)
+    {
+        final Set<String> ret = Sets.newHashSet(set);
+        ret.removeAll(entries.keySet());
+        return ret;
+    }
+
+    public Collection<T> valuesForKeys(final Set<String> keys)
+    {
+        final Map<String, T> map = Maps.newHashMap(entries);
+        map.keySet().retainAll(keys);
+        return map.values();
+    }
+
+}
