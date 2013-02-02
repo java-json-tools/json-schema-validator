3e4d35ffcc4329b8ad86f05b2f23b65eaccd4d6a
Francis Galiegue <fgaliegue@gmail.com>
2013-02-03 00:19:57 +0100

    Delete an unused class
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/com/github/fge/jsonschema/util/ProcessingCache.java
+++ src/main/java/com/github/fge/jsonschema/util/ProcessingCache.java
@@ -25,7 +25,6 @@ import com.google.common.cache.LoadingCache;
 
 import java.util.concurrent.ExecutionException;
 
-// TODO: test
 public final class ProcessingCache<K, V>
 {
     private final LoadingCache<Equivalence.Wrapper<K>, V> cache;
