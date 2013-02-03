6087f09669549e0704f6eb49000df1b232f18848
Francis Galiegue <fgaliegue@gmail.com>
2013-02-03 16:26:00 +0100

    Create skeleton classes for validation processor and keyword validators
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

---
+++ src/main/java/com/github/fge/jsonschema/keyword/KeywordValidator.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.fge.jsonschema.keyword;
+
+import com.github.fge.jsonschema.processing.ProcessingException;
+import com.github.fge.jsonschema.processing.validation.ValidationProcessor;
+import com.github.fge.jsonschema.report.ProcessingReport;
+import com.github.fge.jsonschema.tree.JsonTree;
+
+public interface KeywordValidator
+{
+    void validate(final ValidationProcessor processor,
+        final ProcessingReport report, final JsonTree instance)
+        throws ProcessingException;
+}
---
+++ src/main/java/com/github/fge/jsonschema/processing/validation/ValidationProcessor.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.fge.jsonschema.processing.validation;
+
+import com.github.fge.jsonschema.processing.ProcessingException;
+import com.github.fge.jsonschema.processing.Processor;
+import com.github.fge.jsonschema.processing.ValidationData;
+import com.github.fge.jsonschema.report.ProcessingReport;
+
+public final class ValidationProcessor
+    implements Processor<ValidationData, ProcessingReport>
+{
+    @Override
+    public ProcessingReport process(final ProcessingReport report,
+        final ValidationData input)
+        throws ProcessingException
+    {
+        return null;
+    }
+}
