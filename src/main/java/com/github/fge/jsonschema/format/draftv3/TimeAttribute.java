f6ac5774255d182f0b606f8e99c7168ea0256f2a
Francis Galiegue <fgaliegue@gmail.com>
2013-04-02 20:26:20 +0200

    Reimplement "time" format attribute with DateTimeFormatterBuilder
    And add tests to confirm that it works correctly.
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/com/github/fge/jsonschema/format/draftv3/TimeAttribute.java
+++ src/main/java/com/github/fge/jsonschema/format/draftv3/TimeAttribute.java
@@ -1,40 +1,38 @@
-/*
- * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Lesser GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * Lesser GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.github.fge.jsonschema.format.draftv3;
 
 import com.github.fge.jsonschema.format.FormatAttribute;
-import com.github.fge.jsonschema.format.helpers.DateFormatAttribute;
+import com.github.fge.jsonschema.format.helpers.AbstractDateFormatAttribute;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
 
-/**
- * Validator for the {@code time} format attribute.
- */
+import static org.joda.time.DateTimeFieldType.*;
+
 public final class TimeAttribute
-    extends DateFormatAttribute
+    extends AbstractDateFormatAttribute
 {
     private static final FormatAttribute INSTANCE = new TimeAttribute();
+
+    private TimeAttribute()
+    {
+        super("time", "HH:mm:ss");
+    }
 
     public static FormatAttribute getInstance()
     {
         return INSTANCE;
     }
 
-    private TimeAttribute()
+    @Override
+    protected DateTimeFormatter getFormatter()
     {
-        super("time", "HH:mm:ss");
+        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+
+        builder = builder.appendFixedDecimal(hourOfDay(), 2)
+            .appendLiteral(':')
+            .appendFixedDecimal(minuteOfHour(), 2)
+            .appendLiteral(':')
+            .appendFixedDecimal(secondOfMinute(), 2);
+
+        return builder.toFormatter();
     }
 }
---
+++ src/main/java/com/github/fge/jsonschema/format/helpers/AbstractDateFormatAttribute.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.github.fge.jsonschema.format.helpers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.NodeType;
+import com.github.fge.jsonschema.exceptions.ProcessingException;
+import com.github.fge.jsonschema.format.AbstractFormatAttribute;
+import com.github.fge.jsonschema.processors.data.FullData;
+import com.github.fge.jsonschema.report.ProcessingReport;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.text.SimpleDateFormat;
+
+import static com.github.fge.jsonschema.messages.FormatMessages.*;
+
+/**
+ * Abstract class for date/time related format attributes
+ *
+ * <p><a href="http://joda-time.sourceforge.net/">Joda Time</a> is used for
+ * date and time parsing: it can handle all defined formats, and catches more
+ * errors than the standard JDK's {@link SimpleDateFormat} does.</p>
+ *
+ * <p>Furthermore (and more importantly), unlike {@link SimpleDateFormat}, Joda
+ * Time's {@link DateTimeFormatter} is thread-safe!</p>
+ */
+public abstract class AbstractDateFormatAttribute
+    extends AbstractFormatAttribute
+{
+    private final String format;
+
+    protected AbstractDateFormatAttribute(final String fmt, final String format)
+    {
+        super(fmt, NodeType.STRING);
+        this.format = format;
+    }
+
+    protected abstract DateTimeFormatter getFormatter();
+
+    @Override
+    public final void validate(final ProcessingReport report,
+        final FullData data)
+        throws ProcessingException
+    {
+        final JsonNode instance = data.getInstance().getNode();
+        final DateTimeFormatter formatter = getFormatter();
+
+        try {
+            formatter.parseDateTime(instance.textValue());
+        } catch (IllegalArgumentException ignored) {
+            report.error(newMsg(data, INVALID_DATE_FORMAT)
+                .put("expected", format));
+        }
+    }
+}
