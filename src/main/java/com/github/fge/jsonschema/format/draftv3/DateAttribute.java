c016ddd214e0f7faa0fc447c924e34667429ab4e
Francis Galiegue <fgaliegue@gmail.com>
2013-04-02 20:26:20 +0200

    Reimplement "date" format attribute with DateTimeFormatterBuilder
    And add tests to confirm that it works correctly.
    
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- src/main/java/com/github/fge/jsonschema/format/draftv3/DateAttribute.java
+++ src/main/java/com/github/fge/jsonschema/format/draftv3/DateAttribute.java
@@ -1,40 +1,38 @@
-/*
- * Copyright (c) 2013, Francis Galiegue <fgaliegue@gmail.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the Lesser GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * Lesser GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 package com.github.fge.jsonschema.format.draftv3;
 
 import com.github.fge.jsonschema.format.FormatAttribute;
-import com.github.fge.jsonschema.format.helpers.DateFormatAttribute;
+import com.github.fge.jsonschema.format.helpers.AbstractDateFormatAttribute;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
 
-/**
- * Validator for the {@code date} format attribute.
- */
+import static org.joda.time.DateTimeFieldType.*;
+
 public final class DateAttribute
-    extends DateFormatAttribute
+    extends AbstractDateFormatAttribute
 {
     private static final FormatAttribute INSTANCE = new DateAttribute();
+
+    private DateAttribute()
+    {
+        super("date", "yyyy-MM-dd");
+    }
 
     public static FormatAttribute getInstance()
     {
         return INSTANCE;
     }
 
-    private DateAttribute()
+    @Override
+    protected DateTimeFormatter getFormatter()
     {
-        super("date", "yyyy-MM-dd");
+        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
+
+        builder = builder.appendFixedDecimal(year(), 4)
+            .appendLiteral('-')
+            .appendFixedDecimal(monthOfYear(), 2)
+            .appendLiteral('-')
+            .appendFixedDecimal(dayOfMonth(), 2);
+
+        return builder.toFormatter();
     }
 }
