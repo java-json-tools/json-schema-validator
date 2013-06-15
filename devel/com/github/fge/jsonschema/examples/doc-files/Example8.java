6fd330fd92fa3f6c7c82acc4e319d677400346fb
Francis Galiegue <fgaliegue@gmail.com>
2013-06-16 00:08:03 +0200

    Javadoc for version 2.1.5
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- devel/com/github/fge/jsonschema/examples/doc-files/Example8.java
+++ devel/com/github/fge/jsonschema/examples/doc-files/Example8.java
@@ -28,8 +28,13 @@ import com.github.fge.jsonschema.library.DraftV4Library;
 import com.github.fge.jsonschema.library.Library;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import com.github.fge.jsonschema.messages.JsonSchemaValidationBundle;
 import com.github.fge.jsonschema.processors.data.FullData;
 import com.github.fge.jsonschema.report.ProcessingReport;
+import com.github.fge.msgsimple.bundle.MessageBundle;
+import com.github.fge.msgsimple.serviceloader.MessageBundleFactory;
+import com.github.fge.msgsimple.source.MapMessageSource;
+import com.github.fge.msgsimple.source.MessageSource;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -73,12 +78,30 @@ public final class Example8
         final JsonNode good = Utils.loadResource("/custom-fmt-good.json");
         final JsonNode bad = Utils.loadResource("/custom-fmt-bad.json");
 
+        /*
+         * Build a new library with our added format attribute
+         */
         final Library library = DraftV4Library.get().thaw()
             .addFormatAttribute("uuid", UUIDFormatAttribute.getInstance())
             .freeze();
 
+        /*
+         * Build a new message bundle with our added error message
+         */
+        final String key = "invalidUUID";
+        final String value = "input is not a valid UUID";
+        final MessageSource source = MapMessageSource.newBuilder()
+            .put(key, value).build();
+        final MessageBundle bundle
+            = MessageBundleFactory.getBundle(JsonSchemaValidationBundle.class)
+            .thaw().appendSource(source).freeze();
+
+        /*
+         * Build our dedicated validation configuration
+         */
         final ValidationConfiguration cfg = ValidationConfiguration.newBuilder()
-            .setDefaultLibrary("http://my.site/myschema#", library).freeze();
+            .setDefaultLibrary("http://my.site/myschema#", library)
+            .setValidationMessages(bundle).freeze();
 
         final JsonSchemaFactory factory = JsonSchemaFactory.newBuilder()
             .setValidationConfiguration(cfg).freeze();
@@ -111,14 +134,15 @@ public final class Example8
         }
 
         @Override
-        public void validate(final ProcessingReport report, final FullData data)
+        public void validate(final ProcessingReport report,
+            final  MessageBundle bundle, final FullData data)
             throws ProcessingException
         {
             final String value = data.getInstance().getNode().textValue();
             try {
                 UUID.fromString(value);
             } catch (IllegalArgumentException ignored) {
-                report.error(newMsg(data, "input is not a valid UUID")
+                report.error(newMsg(data, bundle, "invalidUUID")
                     .put("input", value));
             }
         }
