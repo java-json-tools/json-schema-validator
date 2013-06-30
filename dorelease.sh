e64ba40c64469a3b00798fcb8d3d312366092a41
Francis Galiegue <fgaliegue@gmail.com>
2013-06-30 21:24:46 +0200

    dorelease.sh: use Gradle
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

--- dorelease.sh
+++ dorelease.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-#
-# This will build everything that is needed and push to Maven central.
-#
-# The only thing missing is making this script non interactive...
-#
+./gradlew --recompile-scripts clean test uploadArchives
 
-mvn clean package gpg:sign repository:bundle-create deploy
