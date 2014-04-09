f60fd3ffa860b2c812ba7aac585756d4d3cee8e0
Francis Galiegue <fgaliegue@gmail.com>
2014-04-09 08:30:09 +0200

    Really add hyperschema library
    Signed-off-by: Francis Galiegue <fgaliegue@gmail.com>

---
+++ src/main/java/com/github/fge/jsonschema/library/DraftV4HyperSchemaLibrary.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2014, Francis Galiegue (fgaliegue@gmail.com)
+ *
+ * This software is dual-licensed under:
+ *
+ * - the Lesser General Public License (LGPL) version 3.0 or, at your option, any
+ *   later version;
+ * - the Apache Software License (ASL) version 2.0.
+ *
+ * The text of both licenses is available under the src/resources/ directory of
+ * this project (under the names LGPL-3.0.txt and ASL-2.0.txt respectively).
+ *
+ * Direct link to the sources:
+ *
+ * - LGPL 3.0: https://www.gnu.org/licenses/lgpl-3.0.txt
+ * - ASL 2.0: http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package com.github.fge.jsonschema.library;
+
+import com.github.fge.jsonschema.core.keyword.syntax.dictionaries.DraftV4HyperSchemaSyntaxCheckerDictionary;
+import com.github.fge.jsonschema.library.digest.DraftV4DigesterDictionary;
+import com.github.fge.jsonschema.library.format.DraftV4FormatAttributesDictionary;
+import com.github.fge.jsonschema.library.validator.DraftV4ValidatorDictionary;
+
+/**
+ * Library of all draft v4 core schema keywords and format attributes
+ */
+public final class DraftV4HyperSchemaLibrary
+{
+    private static final Library LIBRARY = new Library(
+        DraftV4HyperSchemaSyntaxCheckerDictionary.get(),
+        DraftV4DigesterDictionary.get(),
+        DraftV4ValidatorDictionary.get(),
+        DraftV4FormatAttributesDictionary.get()
+    );
+
+    private DraftV4HyperSchemaLibrary()
+    {
+    }
+
+    public static Library get()
+    {
+        return LIBRARY;
+    }
+}
