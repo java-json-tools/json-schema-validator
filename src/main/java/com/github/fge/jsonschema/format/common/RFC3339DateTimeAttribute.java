d273e06cc4e604ceb833dc6fae9c8123fb155c1d
Tuan Dinh <dvtuan1610@gmail.com>
2017-06-14 23:26:43 +1000

    Used joda-time lib for RFC3339DateTimeAttribute. Added more unit tests

--- src/main/java/com/github/fge/jsonschema/format/common/RFC3339DateTimeAttribute.java
+++ src/main/java/com/github/fge/jsonschema/format/common/RFC3339DateTimeAttribute.java
@@ -1,10 +1,10 @@
 package com.github.fge.jsonschema.format.common;
 
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.format.DateTimeParseException;
-import java.time.temporal.ChronoField;
 import java.util.List;
+
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.DateTimeFormatterBuilder;
+import org.joda.time.format.DateTimeParser;
 
 import com.github.fge.jackson.NodeType;
 import com.github.fge.jsonschema.core.exceptions.ProcessingException;
@@ -22,21 +22,22 @@ import com.google.common.collect.ImmutableList;
 public class RFC3339DateTimeAttribute extends AbstractFormatAttribute {
 
 	private static final List<String> RFC3339_FORMATS = ImmutableList.of(
-	        "yyyy-MM-dd'T'HH:mm:ss((+|-)HH:mm|Z)", "yyyy-MM-dd'T'HH:mm:ss.[0-9]{1,9}((+|-)HH:mm|Z)"
+	        "yyyy-MM-dd'T'HH:mm:ss((+|-)HH:mm|Z)", "yyyy-MM-dd'T'HH:mm:ss.[0-9]{1,12}((+|-)HH:mm|Z)"
 	    );
 	
-    private static final DateTimeFormatter RFC3339_FORMATTER;
+    private static final DateTimeFormatter FORMATTER;
 
     static {
-        final DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder()
-                .appendPattern("yyyy-MM-dd")
-                .appendLiteral('T')
-                .appendPattern("HH:mm:ss")
-                .optionalStart()
-                .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true).parseDefaulting(ChronoField.NANO_OF_SECOND, 0)
-                .optionalEnd()
-                .appendOffset("+HH:mm", "Z");
-        RFC3339_FORMATTER = builder.toFormatter();
+        final DateTimeParser secFracsParser = new DateTimeFormatterBuilder()
+                .appendLiteral('.').appendFractionOfSecond(1,12)
+                .toParser();
+
+        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder()
+                .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
+                .appendOptional(secFracsParser)
+                .appendTimeZoneOffset("Z", true, 2, 2);
+
+        FORMATTER = builder.toFormatter();
     }
 
     private static final FormatAttribute INSTANCE = new RFC3339DateTimeAttribute();
@@ -58,11 +59,50 @@ public class RFC3339DateTimeAttribute extends AbstractFormatAttribute {
     {
         final String value = data.getInstance().getNode().textValue();
 
-        try {
-            RFC3339_FORMATTER.parse(value);
-        } catch (DateTimeParseException ignored) {
-            report.error(newMsg(data, bundle, "err.format.invalidDate")
-                .putArgument("value", value).putArgument("expected", RFC3339_FORMATS));
+        try 
+        {
+            FORMATTER.parseDateTime(value);
+
+            final String secFracsAndOffset = value.substring("yyyy-MM-ddTHH:mm:ss".length());
+            final String offset;
+            if (!secFracsAndOffset.startsWith(".")) {
+            	offset = secFracsAndOffset;
+            } else{
+            	if  (secFracsAndOffset.contains("Z")) {
+            		offset = secFracsAndOffset.substring(secFracsAndOffset.indexOf("Z"));
+            	} else if (secFracsAndOffset.contains("+")) {
+            		offset = secFracsAndOffset.substring(secFracsAndOffset.indexOf("+"));
+            	} else { 
+            		offset = secFracsAndOffset.substring(secFracsAndOffset.indexOf("-"));
+            	}
+            }
+            if (!isOffSetStrictRFC3339(offset)) {
+            	throw new IllegalArgumentException();
+            }
+            
+        } catch (IllegalArgumentException ignored) {
+    		report.error(newMsg(data, bundle, "err.format.invalidDate")
+			    .putArgument("value", value).putArgument("expected", RFC3339_FORMATS));
         }
+        
     }
+
+    /**
+     * Return true if date-time offset stricly follows RFC3339:
+     * <code>time-hour       = 2DIGIT  ; 00-23</code>
+     * <code>time-minute     = 2DIGIT  ; 00-59</code>
+     * <code>time-numoffset  = ("+" / "-") time-hour ":" time-minute</code>
+     * <code>time-offset     = "Z" / time-numoffset</code>,
+     * and false otherwise
+     * @param offset
+     * @return
+     */
+    private boolean isOffSetStrictRFC3339(final String offset) 
+    {
+		if (offset.endsWith("Z")) return true;
+		if (offset.length() == 6 && offset.contains(":")) {
+			return true;
+		}
+		return false;
+	}
 }
